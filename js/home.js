9daac76b798b2a4c6726f05636dc109c64d0bc8a
bhavya-3 <60618734+bhavya-3@users.noreply.github.com>
2020-12-09 19:57:17 +0530

    new changes

--- js/home.js
+++ js/home.js
@@ -2,13 +2,13 @@ if(document.getElementById("thou")){
 	var count = 0;
 	var Timer = setInterval(function(){
 		if(count >= 1000){
-			clearInterval(Timer);
-			document.getElementById("thou").innerHTML = "1000+";
+			count = 500;
+			ddocument.getElementById("fort").innerHTML = count + "+";
 		} else {
 			document.getElementById("thou").innerHTML = count + "+";
 		}
 		count += 1;
-	}, 1);
+	}, 50);
 }
 
 
@@ -16,25 +16,27 @@ if(document.getElementById("thou2")){
 	var count2 = 0;
 	var Timer2 = setInterval(function(){
 		if(count2 >= 1000){
-			clearInterval(Timer2);
-			document.getElementById("thou2").innerHTML = "1000+";
+			count2 = 500;
+			ddocument.getElementById("fort").innerHTML = count2 + "+";
 		} else {
 			document.getElementById("thou2").innerHTML = count2 + "+";
 		}
 		count2 += 1;
-	}, 1);
+	}, 50);
 }
 
 
 if(document.getElementById("fort")){
 	var count3 = 0;
 	var Timer3 = setInterval(function(){
-		if(count3 > 40){
-			clearInterval(Timer3);
-			document.getElementById("fort").innerHTML = "40+";
+		if(count3 == 41){
+			count3 = 20;
+			ddocument.getElementById("fort").innerHTML = count3 + "+";
+
 		} else {
 			document.getElementById("fort").innerHTML = count3 + "+";
 		}
+
 		count3 += 1;
 	}, 100);
 }
